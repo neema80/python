015d86d59691313a44532a6e31e1117a1d9c6047
Nima Bahramzadeh <neema80@gmail.com>
2020-03-06 01:03:47 +0330

    added older files 03.py and for_loop.py along with first_jup

---
+++ 03.py
@@ -0,0 +1,54 @@
+__author__ = "Nima"
+
+a = 10
+b = 25
+c = 100
+d = ("cat", "dog", "bird")
+if a == 100 and b < 45 :
+    print("This is True!")
+else :
+    print("a is {} so This is false!".format(a))
+if a == 10 : # nested logical statement
+    # as the python is interpreter is it important if the condition is not true then it will ignore the rest
+    # automatically
+    print("a is true!")
+    if b < 45 :
+        print("b is True!")
+        if c == 100 :
+            print("c is true!")
+            if "zebra" in d :
+                print("d is true!")
+            else:
+                print("d is false!")
+        else :
+            print("c is false!")
+    else :
+        print("b is false!")
+else :
+    print("a is false!")
+
+# num = eval(input("Enter a number: ")) # by default the format is tring if we want to calculate it we have to change the format
+                        # using the eval function
+#print("number is {}".format(num), " type is: ", type(num))
+#
+# now we use this input for searching in the touple
+#if num >= 3 :
+#    print("Keep Looking!")
+#elif d[num] == "cat" :
+#    print("Found a {}".format(d[num]))
+#elif d[num] == "dog" :
+#    print("Found a {}".format(d[num]))
+#else :
+#    print("Found a {}".format(d[num]))
+
+list = [list(range(11)),list(range(11,21)),list(range(21,31))]
+print(list)
+v = eval(input("Enter a number: "))
+if v in list[0] :
+    print(v, " is in list one")
+elif v in list[1] :
+    print(v, "is in list two")
+elif v in list[2]:
+    print(v, "is in list three")
+else :
+    print(v, "is not in the list!")
---
+++ for_loop.py
@@ -0,0 +1,47 @@
+for i in range(0,20,2) :
+    print(i)
+cats = ["tiger", "lion", "jaguar", "leopard"]
+for c in cats :
+    print(c)
+    # it prints out the elemts in the list
+    # similar to cats[c]
+
+nest1 = [[10, 20, 30],[3.5, 4.5, 5.5],["sword","hammer","shield"]]
+for i in range(3) :
+    res = nest1[0][i] + nest1[1][i]
+    print(nest1[2][i].capitalize() + ": Power Level =", res)
+    print(nest1[2][i].upper() , ": Power Level =", res)
+    # if we use + in the print func it concatenates the strings together
+    # while if we use , it puts a space between strings
+
+# reformatting of the above list
+weapons = []
+for i in range(3) :
+    weapons.append(nest1[2][i].capitalize() + ": Power Level = " + str(nest1[0][i] + nest1[1][i]))
+print(weapons)
+items = eval(input("Enter Weapons item: "))
+if items >= 0 and items <= 2 :
+    print(weapons[items])
+else :
+    print("Item not found!!")
+
+for i in range(1, 8) :
+    print("{} + {} = {}".format(i,i,(i+i)))
+for i in range(1, 8) :
+    print("%d * %f = %f" % (i , i , (i*i)))
+    # %d means integer
+    # %f means float
+
+# touple unpacking
+tup1 = ((10, 20), ("a", "b"),(100, 200))
+for i, j in tup1 :
+    print(i,j)
+
+cakes = ["chocolate", "lemon", "carrot", "vanilla"]
+d1 = {"k1": tuple(cakes), "k2" : [10,20,30,40]}
+for i in d1 :
+    # print(i)
+    # prints out the key elemts
+    # for printing the elemets and values we use nested for loop
+    for j in range(0,len(cakes)) :
+        print(i ,":", d1[i][j])
