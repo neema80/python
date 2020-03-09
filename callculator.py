fa568f38723c0f16f9be24768c14fff19aedb126
Neema <neema80@gmail.com>
2020-03-09 22:56:36 +0330

    calculator app edited and updated

--- callculator.py
+++ callculator.py
@@ -15,25 +15,25 @@ while True:
     if choose == 'quit':
         break
     elif choose == 'add':
-        val1 = float(input(print('entered first value: ')))
+        val1 = float(input(print('enter first value: ')))
         val2 = float(input(print('second value: ')))
         result = val1 + val2
     elif choose == 'division':
-        val1 = float(input(print("entered first value: ")))
+        val1 = float(input(print("enter first value: ")))
         val2 = float(input(print("second value: ")))
         result = val1 / val2
     elif choose == 'minus':
-        val1 = float(input(print("entered first value: ")))
+        val1 = float(input(print("enter first value: ")))
         val2 = float(input(print("second value: ")))
         result = val1 - val2
     elif choose == 'floor':
-        val1 = float(input(print("entered first value: ")))
+        val1 = float(input(print("enter first value: ")))
         val2 = float(input(print("second value: ")))
         result = val1 // val2
     elif choose == 'exponentiation':
-        val1 = float(input(print("entered first value: ")))
+        val1 = float(input(print("enter first value: ")))
         val2 = float(input(print("second value: ")))
         result = val1 ** val2
     else:
-        print("Command not found!")
+        print("Command could not found!")
     print("total = ", result)
