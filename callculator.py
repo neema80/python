e054e97b41afbed9075c3a12ba0289978b0bcc13
Neema <neema80@gmail.com>
2020-03-09 22:46:54 +0330

    calulator app updated

--- callculator.py
+++ callculator.py
@@ -4,15 +4,36 @@
 
 while True:
     print(""" WELCOME TO CALCULATOR APP:
-    To do the add enter 'add' 
+    To do the add enter 'add'
+    for minus enter 'minus' 
     for multiplication enter 'multiply'
     for division enter 'division'
     for floor division and remainder enter 'floor'
+    for power enter 'exponentiation'
     or 'quit' to end this app""")
-    choose = input("Please choose your action from the above:")
+    choose = input("Please choose your action from the above: ")
     if choose == 'quit':
         break
     elif choose == 'add':
-        print("add entered")
+        val1 = float(input(print('entered first value: ')))
+        val2 = float(input(print('second value: ')))
+        result = val1 + val2
+    elif choose == 'division':
+        val1 = float(input(print("entered first value: ")))
+        val2 = float(input(print("second value: ")))
+        result = val1 / val2
+    elif choose == 'minus':
+        val1 = float(input(print("entered first value: ")))
+        val2 = float(input(print("second value: ")))
+        result = val1 - val2
+    elif choose == 'floor':
+        val1 = float(input(print("entered first value: ")))
+        val2 = float(input(print("second value: ")))
+        result = val1 // val2
+    elif choose == 'exponentiation':
+        val1 = float(input(print("entered first value: ")))
+        val2 = float(input(print("second value: ")))
+        result = val1 ** val2
     else:
         print("Command not found!")
+    print("total = ", result)
