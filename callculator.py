72210eb66cfc6e016089420b6932ed044e797bb2
Neema <neema80@gmail.com>
2020-03-09 21:59:45 +0330

    calulator app updated

--- callculator.py
+++ callculator.py
@@ -1,2 +1,18 @@
+#
+# print("total = ", eval(input("enter some text: ")))
+# the calculator app versio 1.0
 
-print("total = ", eval(input("enter some text: ")))
+while True:
+    print(""" WELCOME TO CALCULATOR APP:
+    To do the add enter 'add' 
+    for multiplication enter 'multiply'
+    for division enter 'division'
+    for floor division and remainder enter 'floor'
+    or 'quit' to end this app""")
+    choose = input("Please choose your action from the above:")
+    if choose == 'quit':
+        break
+    elif choose == 'add':
+        print("add entered")
+    else:
+        print("Command not found!")
