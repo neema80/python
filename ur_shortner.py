e246523093432deddb9f38d6d5fb52e5dd784c82
Nima Bahramzadeh <neema80@gmail.com>
2020-03-13 22:50:49 +0330

    ur_shortner updated

--- ur_shortner.py
+++ ur_shortner.py
@@ -1,4 +1,4 @@
-import pyshortners
+import pyshorteners
 import tkinter as tk
 from tkinter import ttk
 from tkinter.messagebox import showinfo
@@ -8,7 +8,7 @@ win.title("URL Shortner")
 
 def short():
     url = entry_url.get()
-    shorted_url = pyshortners.Shortner()
+    shorted_url = pyshorteners.Shortener()
     showinfo("Shorted URL",f"{shorted_url.tinyurl.short(url)}")
 
 url_label = tk.Label(win,text="Enter URL Here :  ")
@@ -17,7 +17,7 @@ url_label.grid(row=0,column=0)
 entry_url = tk.Entry(win,width=30,bg="#69BEF6", bd=2)
 entry_url.grid(row=0,column=1,padx=5,pady=5)
 
-button = ttk.button(win,text="Short",command=short)
+button = ttk.Button(win,text="Short",command=short)
 button.grid(row=1,column=0,columnspan=2)
 
 win.mainloop()
