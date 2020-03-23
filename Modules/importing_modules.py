dfed5496b65e525446abfa3bbb55bfeeb85d6799
Neema Bahramzadeh <neema80@gmail.com>
2020-03-24 00:16:18 +0430

    updated module chapter

--- Modules/importing_modules.py
+++ Modules/importing_modules.py
@@ -1,7 +1,7 @@
 # if we want to see how many functions are inside a module file after import we can
 # use the dir command
 import math
-print(dir(math))
+print(dir(math)) # or can use print(help(math))
 print(type(math.pi), math.pi) # this is a pi variable inside math module
 
 # to make file uses less resources we can use from ... import ...
@@ -10,3 +10,6 @@ print(type(math.pi), math.pi) # this is a pi variable inside math module
 # to check what is the path variable of the system we can use as below
 import sys
 print(sys.path)
+
+# to upgrape pip module itself we should run the code below in CMD line
+# python -m pip install --upgrade pip
