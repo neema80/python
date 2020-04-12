abac0fc153abcf5c4c5facdfa04c6b961c4b4f83
Nima Bahramzadeh <neema80@gmail.com>
2020-04-12 13:22:18 +0430

    gussing game update

--- automation/outside_python/firstscript.py
+++ automation/outside_python/firstscript.py
@@ -1,5 +1,27 @@
 #! python3
 import sys
+import random
+import time
 
-print('Hello World!')
-print(sys.argv)
+random.seed(time.time()) 
+number = random.randint(1,20)
+# print('Hello World!')
+# print(sys.argv)
+user_name = input('What is your name? ')
+print('Hello {}, I am gussing number between 1 and 20. '.format(user_name))
+count = 1
+
+while True:
+    guess = int(input('Guess a number: '))
+    if count >= 10:
+        print('Sorry you could not guess it in 10 rounds! you lost!')
+        break
+    if guess > number:
+        print('You are guessing high!')
+        count += 1
+    elif guess < number:
+        print('You are guessing low!')
+        count += 1
+    else:
+        print('You guessed correct {} in {} guesses'.format(user_name,count))
+        break
