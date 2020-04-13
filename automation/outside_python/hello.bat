f3985693e8731b000b9a92cd7be1c56ebebb8696
Neema Bahramzadeh <neema80@gmail.com>
2020-04-14 02:30:39 +0430

    updated hello.bat file to work with Lenovo machine

--- automation/outside_python/hello.bat
+++ automation/outside_python/hello.bat
@@ -1,4 +1,5 @@
+@echo off
 REM @ is to hide the command prompt
 REM %* means to pass every argument that came from the user to the firstscript.py file
-@py "C:\Users\Nima Bahramzadeh\OneDrive\Development Lab\Github\python\automation\outside_python\firstscript.py" %*
+@py "C:\Users\Nima\OneDrive\Development Lab\Github\python\automation\outside_python\firstscript.py" %*
 @pause
