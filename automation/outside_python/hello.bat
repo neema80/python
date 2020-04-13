c507750662b9e73feb428874c2776062f60329ce
Neema Bahramzadeh <neema80@gmail.com>
2020-04-14 02:27:44 +0430

    minor changes to hello.bat file

--- automation/outside_python/hello.bat
+++ automation/outside_python/hello.bat
@@ -1,2 +1,4 @@
+REM @ is to hide the command prompt
+REM %* means to pass every argument that came from the user to the firstscript.py file
 @py "C:\Users\Nima Bahramzadeh\OneDrive\Development Lab\Github\python\automation\outside_python\firstscript.py" %*
 @pause
