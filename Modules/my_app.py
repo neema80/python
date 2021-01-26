65baeb3bbc43ea9d0c54719b667fe6372760140f
Nima Bahramzadeh <46886194+neema80@users.noreply.github.com>
2021-01-26 11:51:48 +0000

    Delete Modules directory

--- Modules/my_app.py
+++
@@ -1,3 +0,0 @@
-import my_module # as the module is readed before everything
-# the codes inside that file will be executed before everything else
-print("This is a simple statement printed inside my_app name")
